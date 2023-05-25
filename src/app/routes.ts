457ee3beaaec3eaff49635f547ae519a69fcdef5
Berny Itoutou <berny-francois@cosavostra.com>
2023-05-25 11:29:38 +0200

    feat(views):add error page to the routes

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,12 +1,14 @@
 import {createBrowserRouter} from 'react-router-dom';
 import Home from '@views/Home/home';
+import ErrorPage from "@views/Error-Page/error-page.tsx";
 
 
 const routes = createBrowserRouter([
     {
         path: '/',
-        element: <Home/>,
-    }
+        element: <Home />,
+        errorElement: <ErrorPage/>
+    },
 ])
 
 export default routes;
